2bd91560d15e4b0e80e2db48edbce9e6175c8521
Masafumi Misawa <m.masafumi@wano.co.jp>
2025-04-23 20:27:24 +0900

    add Plugin "avante", "git-conflict"

--- nvim/README.md
+++ nvim/README.md
@@ -2,3 +2,9 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+- Editor: Neovim
+- Plugin Manager: Lazy.vim
+- Plugins
+  - avante.nvim // to emulate the behaviour of the Cursor AI IDE
+  - git-conflict.nvim // to visualise and resolve conflicts in neovim
---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,19 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	build = "make",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+		"zbirenbaum/copilot.lua", -- for providers='copilot'
+	},
+	opts = {
+		provider = "copilot",
+	},
+}
