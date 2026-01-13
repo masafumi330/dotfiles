22cda04d0f84d7fc30dab7a9c9c2f800cfc2cc00
Masafumi Misawa <m.masafumi@wano.co.jp>
2026-01-14 00:59:39 +0900

    add blink.cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,8 +4,18 @@ local perlnavigator = require("plugins.lsp.perlnavigator")
 return {
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
 		opts = {
 			servers = vim.tbl_deep_extend("force", gopls, perlnavigator),
 		},
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			for server, config in pairs(opts.servers) do
+				config.capabilities = capabilities
+				lspconfig[server].setup(config)
+			end
+		end,
 	},
 }
