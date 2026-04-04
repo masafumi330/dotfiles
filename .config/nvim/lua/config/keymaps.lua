ed7708660f0596b7a36298d09c82cb0c2452a379
masafumi330 <m.masafumi.dev@gmail.com>
2026-04-04 17:39:23 +0900

    delete old .config dir

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Insertモードで jj を ESC にマップ
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "q", "<nop>")
