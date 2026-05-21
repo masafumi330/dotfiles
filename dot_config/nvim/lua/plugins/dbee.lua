f4995c46a0ee0f2641cd9449f85984caa4a6a490
masafumi330 <m.masafumi.dev@gmail.com>
2026-05-21 14:57:17 +0900

    add plugin: nvim-dbee

--- dot_config/nvim/lua/plugins/dbee.lua
+++ dot_config/nvim/lua/plugins/dbee.lua
@@ -1,15 +1,21 @@
 return {
   "kndndrj/nvim-dbee",
+  lazy = true,
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
   build = function()
     require("dbee").install()
   end,
-  config = function()
-    require("dbee").setup()
-  end,
+  cmd = { "Dbee" },
   keys = {
     { "<leader>D", function() require("dbee").toggle() end, desc = "Toggle DBee" },
   },
+  config = function()
+    require("dbee").setup({
+      sources = {
+        require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
+      },
+    })
+  end,
 }
